1abfed8328e53bed76cebab5ba15bbfe6fd11e44
ShichinoheRina <116484656+ShichinoheRina@users.noreply.github.com>
2022-12-15 23:26:14 +0900

    sqlのデータ追加

---
+++ gs_1210.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: 127.0.0.1
+-- 生成日時: 2022-12-15 15:21:41
+-- サーバのバージョン： 10.4.27-MariaDB
+-- PHP のバージョン: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `gs_1210`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `gs_bm_table`
+--
+
+CREATE TABLE `gs_bm_table` (
+  `id` int(12) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `url` text NOT NULL,
+  `comment` text NOT NULL,
+  `date` datetime NOT NULL,
+  `picture` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- テーブルのデータのダンプ `gs_bm_table`
+--
+
+INSERT INTO `gs_bm_table` (`id`, `name`, `url`, `comment`, `date`, `picture`) VALUES
+(2, '人間失格', '読書中', 'おもしろい', '2022-12-13 21:49:51', 0),
+(3, '原体験ドリブン', '未読', '読みたい', '2022-12-15 23:07:22', 0),
+(4, '今日の空が一番好き、とまだ言えない僕は', '読了', '面白かった', '2022-12-15 23:08:51', 0),
+(5, '料理本', '未読', '料理のレシピ本', '2022-12-15 23:17:52', 0);
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `gs_bm_table`
+--
+ALTER TABLE `gs_bm_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `gs_bm_table`
+--
+ALTER TABLE `gs_bm_table`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
